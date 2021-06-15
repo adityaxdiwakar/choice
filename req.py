43dc88c42185451403062f960b372d81e18773f9
Aditya Diwakar <aditya@diwakar.io>
2021-06-15 17:01:55 -0400

    :rocket: Add data for 06/15

--- req.py
+++ req.py
@@ -24,31 +24,56 @@ def get_quote(ticker: str, series: list[str], min, max: int) -> dict:
     logger(f"Retrieved chain quote for {ticker}")
     return req.json()
 
-ticker_dir = os.listdir("bin/")
-bar = progressbar.ProgressBar(max_value=len(ticker_dir))
-for i, ticker in enumerate(ticker_dir):
-    with open(f"bin/{ticker}/series.json", "r") as f:
-        series = json.load(f)
+while True:
+    print("")
+    time_added = datetime.today().hour * 100 + datetime.today().minute
+    if time_added > 1601:
+        print("=> Night time: taking a nap for 7,200 seconds... options exchange closed")
+        time.sleep(7200)
+        continue
+    elif time_added <= 900:
+        print("=> Good morning: taking a nap for 900 seconds... options exchange closed")
+        time.sleep(900)
+        continue
+    elif time_addded < 930:
+        print("=> Almost time: taking a nap for 60 seconds... options exchange almost open")
+        time.sleep(60)
+        continue
 
-    names = [d["name"] for d in series]
+    ttw = 60 - datetime.today().second
+    print(f"=> Taking a nap for {ttw} seconds... waiting for next minute to align with")
+    time.sleep(ttw)
+    print(f"=> Aligned, continuing for: {datetime.today().hour}:{str(datetime.today().minute).zfill(2)}")
 
-    v = get_quote(ticker, names, 0.01, 9999)
-    for opt in v["payload"]:
-        e_dir = opt["symbol"][1 + len(ticker): 7 + len(ticker)]
-        strike = opt["symbol"][8 + len(ticker):]
-        today = datetime.today()
-        month = str(today.month).zfill(2)
-        day = str(today.day).zfill(2)
-        hour = str(today.hour).zfill(2)
-        minute = str(today.minute).zfill(2)
 
-        path = f"bin/{ticker}/{e_dir}/{strike}/{opt['symbol']}/{month}/{day}/{hour}"
-        Path(path).mkdir(parents=True, exist_ok=True)
-        with open(f"{path}/{minute}.json", "w") as f:
-            json.dump(opt, f)
+    ticker_dir = os.listdir("bin/")
+    bar = progressbar.ProgressBar(max_value=len(ticker_dir))
+    for i, ticker in enumerate(ticker_dir):
+        with open(f"bin/{ticker}/series.json", "r") as f:
+            series = json.load(f)
 
-        logger(f"Inserted {path}")
+        names = [d["name"] for d in series]
 
-    bar.update(i+1)
+        v = get_quote(ticker, names, 0.01, 9999)
+        if v == None:
+            continue
 
-    time.sleep(2)
+        for opt in v["payload"]:
+            e_dir = opt["symbol"][1 + len(ticker): 7 + len(ticker)]
+            strike = opt["symbol"][8 + len(ticker):]
+            today = datetime.today()
+            month = str(today.month).zfill(2)
+            day = str(today.day).zfill(2)
+            hour = str(today.hour).zfill(2)
+            minute = str(today.minute).zfill(2)
+
+            path = f"bin/{ticker}/{e_dir}/{strike}/{opt['symbol'][1:]}/{month}/{day}/{hour}"
+            Path(path).mkdir(parents=True, exist_ok=True)
+            with open(f"{path}/{minute}.json", "w") as f:
+                json.dump(opt, f)
+
+            logger(f"Inserted {path}")
+
+        bar.update(i+1)
+
+        time.sleep(2)
