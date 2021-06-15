13014cf12fd154541ca3ca103a37e9b058b83132
Aditya Diwakar <aditya@diwakar.io>
2021-06-14 22:12:32 -0400

    :rocket: Finish API

---
+++ api/go.mod
@@ -0,0 +1,10 @@
+module github.com/adityaxdiwakar/choice
+
+go 1.16
+
+require (
+	github.com/adityaxdiwakar/flux v0.0.0-20210615013911-8a8d1ced2dd5 // indirect
+	github.com/adityaxdiwakar/tda-go v0.0.0-20200720003608-6e71f979b4cb // indirect
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/go-chi/chi/v5 v5.0.3 // indirect
+)
---
+++ api/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/adityaxdiwakar/flux"
+	"github.com/adityaxdiwakar/tda-go"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+)
+
+type PayloadResponse struct {
+	Payload interface{} `json:"payload"`
+	Code    int         `json:"status_code"`
+}
+
+func main() {
+	// initialize the tda session
+	tdaSession := tda.Session{
+		Refresh:     REFRESH_TOKEN,
+		ConsumerKey: CONSUMER_KEY,
+		RootUrl:     "https://api.tdameritrade.com/v1",
+	}
+
+	// initialize the flux session
+	s, err := flux.New(tdaSession, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.Open()
+
+	// init the chi router
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/series/{ticker}", func(w http.ResponseWriter, r *http.Request) {
+		sig := flux.OptionSeriesRequestSignature{
+			Ticker: chi.URLParam(r, "ticker"),
+		}
+
+		payload, err := s.RequestOptionSeries(sig)
+
+		if err != nil {
+			encode(err, w, 500)
+			return
+		}
+
+		encode(payload, w, 200)
+	})
+
+	r.Get("/chain/{underlying}/{series}", func(w http.ResponseWriter, r *http.Request) {
+		sig := flux.OptionChainGetRequestSignature{
+			Underlying: chi.URLParam(r, "underlying"),
+			Filter: flux.OptionChainGetFilter{
+				StrikeQuantity: 1<<31 - 1,
+				SeriesNames:    []string{chi.URLParam(r, "series")},
+			},
+		}
+
+		payload, err := s.RequestOptionChainGet(sig)
+
+		if err != nil {
+			encode(err, w, 500)
+			return
+		}
+
+		encode(payload, w, 200)
+	})
+
+	r.Get("/chart/{ticker}/{range}/{width}", func(w http.ResponseWriter, r *http.Request) {
+		sig := flux.ChartRequestSignature{
+			Ticker: chi.URLParam(r, "ticker"),
+			Range:  chi.URLParam(r, "range"),
+			Width:  chi.URLParam(r, "width"),
+		}
+
+		payload, err := s.RequestChart(sig)
+
+		if err != nil {
+			encode(err, w, 500)
+			return
+		}
+
+		encode(payload, w, 200)
+	})
+
+	http.ListenAndServe(":7731", r)
+
+}
+
+func encode(data interface{}, w http.ResponseWriter, statusCode int) {
+	w.WriteHeader(statusCode)
+	w.Header().Set("Content-Type", "application/json")
+	resp := PayloadResponse{
+		Payload: data,
+		Code:    statusCode,
+	}
+	str, _ := json.MarshalIndent(resp, "", "    ")
+	str = bytes.Replace(str, []byte("\\u003c"), []byte("<"), -1)
+	str = bytes.Replace(str, []byte("\\u003e"), []byte(">"), -1)
+	str = bytes.Replace(str, []byte("\\u0026"), []byte("&"), -1)
+	w.Write(str)
+}
